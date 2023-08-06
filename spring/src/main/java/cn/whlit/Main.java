4799b5322e3acd53a6fdb8141e787c6b153773b6
whl <w_w9264@163.com>
2023-08-06 14:08:12 +0800

    spring boot

---
+++ spring/src/main/java/cn/whlit/Main.java
@@ -0,0 +1,15 @@
+package cn.whlit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WangHaiLong 2023/8/5 21:56
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
