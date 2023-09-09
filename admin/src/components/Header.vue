ad5a8c18842f38446b0189c96196bc09d73a32f4
whlit <whlit@aliyun.com>
2023-09-09 20:26:37 +0800

    admin

---
+++ admin/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  Header
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
---
+++ admin/src/components/Sider.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-layout-sider bordered >
+    Sider
+  </n-layout-sider>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
