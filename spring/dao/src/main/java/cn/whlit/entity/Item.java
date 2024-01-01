dc058da01af563ee3a510c76f648925988775001
whl <whlit@aliyun.com>
2024-01-01 18:14:58 +0800

    fix: lombok 与 mybatis 结合时属性顺序问题

--- spring/dao/src/main/java/cn/whlit/entity/Item.java
+++ spring/dao/src/main/java/cn/whlit/entity/Item.java
@@ -1,37 +1,24 @@
 package cn.whlit.entity;
 
 import cn.whlit.ItemType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author WangHaiLong 2024/1/1 16:59
  */
+@Data
+@Builder
+//添加了Builder注解后，就没有了默认的无参构造函数，只有一个全参的，此时将属性的顺序与mybatis中resultMap中定义的顺序更改为不一致时，会导致异常
+@NoArgsConstructor
+//添加NoArgsConstructor注解后导致Builder注解的全参构造函数缺失，报错需要添加一个全参的构造函数
+@AllArgsConstructor
 public class Item {
 
     private Integer id;
     private ItemType type;
     private String name;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ItemType getType() {
-        return type;
-    }
-
-    public void setType(ItemType type) {
-        this.type = type;
-    }
 }
